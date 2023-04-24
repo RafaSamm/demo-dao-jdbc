91338fab79ba9ba2fea69281bd700c4a7b7b8a81
RafaSamm <rafarhs286@gmail.com>
2023-04-24 09:22:07 -0300

    Project created

---
+++ src/Program.java
@@ -0,0 +1,17 @@
+import db.DB;
+
+import java.sql.Connection;
+
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+
+
+
+
+    }
+}

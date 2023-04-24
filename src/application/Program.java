40545dbbf1f7b1d5704e97470ac81bc7908b50d4
RafaSamm <rafarhs286@gmail.com>
2023-04-24 11:29:14 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,5 +15,9 @@ public class Program {
 
     public static void main(String[] args) {
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }

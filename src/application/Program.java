8f1743edb054bd1187562316f208f30bea897c81
RafaSamm <rafarhs286@gmail.com>
2023-04-24 09:57:26 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,16 @@
+package application;
+
 import db.DB;
+import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 
 public class Program {
 
     public static void main(String[] args) {
-
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
-
-
-
-
+        
     }
 }

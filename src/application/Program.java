bb189019299210c677b7a3748e8b2d3527e4a9aa
RafaSamm <rafarhs286@gmail.com>
2023-04-26 09:44:34 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,5 +26,10 @@ public class Program {
         List<Seller> list = sellerDao.findByDepartment(dep);
         list.forEach(System.out::println);
 
+        System.out.println("\n=== Test 3 seller findAll ===");
+        list = sellerDao.findAll();
+        list.forEach(System.out::println);
+
+
     }
 }
